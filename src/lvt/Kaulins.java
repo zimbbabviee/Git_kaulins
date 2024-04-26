03ecda0eda10968feb91d346679fa5d683576024
Jevgenija <jevgenijasenko@lvt.sk.lv>
2024-04-26 08:19:56 +0300

    Pievienots ierobezojums reizu skaits

---
+++ src/lvt/Kaulins.java
@@ -0,0 +1,24 @@
+package lvt;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Kaulins {
+	static void mestKaulinu(int reizes) {
+		int skaitlis;
+		Random rand = new Random();
+		for(int i=1; i<=reizes; i++) {
+			skaitlis = rand.nextInt(6)+1;	
+			System.out.println("Uzkrita skaitlis "+skaitlis);
+		}
+	}
+	
+	public static void main(String[] args) {
+		int reizes;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Cik reizes mest kauliņu?");
+		reizes = scan.nextInt();
+		mestKaulinu(reizes);
+		scan.close();
+	}
+}
